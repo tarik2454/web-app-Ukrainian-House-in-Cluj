634eef0764ce94bdfd1625b42d0efb0c48783c97
Taras Kovalyk <tarik2454@gmail.com>
2024-07-30 19:32:33 +0300

    start project

--- src/App.tsx
+++ src/App.tsx
@@ -1,49 +1,35 @@
-import { Route, Routes } from 'react-router-dom';
-import { lazy } from 'react';
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
-import Layout from './components/Layout';
-const Home = lazy(() => import('./pages/Home/Home'));
-import Welcome from './pages/Welcome/Welcome';
-import NotFound from './pages/NotFound/NotFound';
-import PublicRoute from './HOC/PublicRoute';
+function App() {
+  const [count, setCount] = useState(0)
 
-const App = () => {
   return (
     <>
-      <Routes>
-        <Route path="website-LearnLingo" element={<Layout />}>
-          <Route index element={<Home />} />
-
-          <Route
-            path="welcome"
-            element={
-              <PublicRoute>
-                <Welcome />
-              </PublicRoute>
-            }
-          />
-          <Route
-            path="signin"
-            element={
-              <PublicRoute>
-                <Welcome />
-              </PublicRoute>
-            }
-          />
-          <Route
-            path="signup"
-            element={
-              <PublicRoute>
-                <Welcome />
-              </PublicRoute>
-            }
-          />
-
-          <Route path="*" element={<NotFound />} />
-        </Route>
-      </Routes>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
     </>
-  );
-};
+  )
+}
 
-export default App;
+export default App
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,20 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
+import { fileURLToPath } from 'url';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/website-LearnLingo/',
+  build: {
+    sourcemap: true,
+  },
+  css: {
+    devSourcemap: true,
+  },
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+  base: '/web-app-Ukrainian-House-in-Cluj/',
 });

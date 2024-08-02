2194325a53ee4e902402d6821db9aac1b3fd873a
Taras Kovalyk <tarik2454@gmail.com>
2024-08-02 23:29:50 +0300

    adds changes

--- vite-env.d.ts
+++ vite-env.d.ts
@@ -1,10 +1,1 @@
 /// <reference types="vite/client" />
-
-interface ImportMetaEnv {
-  readonly VITE_BASE_URL: string;
-  // можно добавить и другие переменные окружения, если они у вас есть
-}
-
-interface ImportMeta {
-  readonly env: ImportMetaEnv;
-}
--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,20 @@
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import { fileURLToPath } from 'url';
 
-export default defineConfig(({ mode }) => {
-  // Загрузите соответствующие переменные окружения
-  const env = loadEnv(mode, process.cwd());
-
-  return {
-    base: env.VITE_BASE_URL,
-    plugins: [react()],
-    build: {
-      sourcemap: true,
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    sourcemap: true,
+  },
+  css: {
+    devSourcemap: true,
+  },
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
-    css: {
-      devSourcemap: true,
-    },
-    resolve: {
-      alias: {
-        '@': fileURLToPath(new URL('./src', import.meta.url)),
-      },
-    },
-  };
+  },
+  base: '/web-app-Ukrainian-House-in-Cluj/',
 });

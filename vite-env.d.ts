1df380ed4ca6eeb09334dc7979799d401bb6c465
Taras Kovalyk <tarik2454@gmail.com>
2024-08-02 23:25:02 +0300

    adds route to deploy

--- vite-env.d.ts
+++ vite-env.d.ts
@@ -1,1 +1,10 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_BASE_URL: string;
+  // можно добавить и другие переменные окружения, если они у вас есть
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,24 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import { fileURLToPath } from 'url';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  build: {
-    sourcemap: true,
-  },
-  css: {
-    devSourcemap: true,
-  },
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
+export default defineConfig(({ mode }) => {
+  // Загрузите соответствующие переменные окружения
+  const env = loadEnv(mode, process.cwd());
+
+  return {
+    base: env.VITE_BASE_URL,
+    plugins: [react()],
+    build: {
+      sourcemap: true,
     },
-  },
-  // base: '/web-app-Ukrainian-House-in-Cluj/',
+    css: {
+      devSourcemap: true,
+    },
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
+      },
+    },
+  };
 });

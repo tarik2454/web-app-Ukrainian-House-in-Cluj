b6c318f401741ff827ec5571c29048df4839bfd9
Taras Kovalyk <tarik2454@gmail.com>
2024-08-22 22:41:19 +0300

    fix bug deploy

--- src/shared/components/Logo.tsx
+++ src/shared/components/Logo.tsx
@@ -3,7 +3,7 @@ import routes from '../../constants/routes';
 
 export default function Logo() {
   return (
-    <a href={routes.ROOT}>
+    <a href={routes.ROOT.path}>
       <div className="flex gap-2 items-center">
         <img src={logo} width="50px" height="50px" alt="Logo" />
         <p className="text-ukraine-gradient text-base">Ukraine House</p>

0fe0e3b3a058cf9c526f138f49f1b5f3a8f95d2d
Taras Kovalyk <tarik2454@gmail.com>
2024-08-28 21:10:48 +0300

    fix bug set active class to NavBar

--- src/shared/components/NavBar.tsx
+++ src/shared/components/NavBar.tsx
@@ -1,6 +1,7 @@
-import { NavLink } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
 import useRoutesByKey from '../hooks/useRoutesByKey';
+import { useState } from 'react';
 
 export default function NavBar() {
   const routes = useRoutesByKey([
@@ -10,21 +11,29 @@ export default function NavBar() {
     'EVENTS',
     'ABOUT_US',
   ]);
+  const location = useLocation();
+  const [activePath, setActivePath] = useState(location.pathname);
+
+  const handleClickLink = (path: string) => {
+    setActivePath(path);
+  };
 
   return (
     <nav>
       <ul className="flex gap-[30px]">
-        {routes.map((item, index) => (
+        {routes.map(({ path, name }, index) => (
           <li key={index} className="relative">
-            <NavLink
-              className="text-black-200 text-xl 
+            <Link
+              className={`text-black-200 text-xl 
               after:block after:content-[''] after:w-full after:h-[1.5px] after:bg-black-200 after:scale-0 after:absolute after:-bottom-[2px] after:transition-all
               hover:after:scale-100 focus:after:scale-100
-              dark:text-dark-title dark:after:bg-dark-title"
-              to={item.path}
+              ${activePath === path ? 'after:scale-100 fter:scale-100' : ''}
+              dark:text-dark-title dark:after:bg-dark-title`}
+              to={path}
+              onClick={() => handleClickLink(path)}
             >
-              {item.name}
-            </NavLink>
+              {name}
+            </Link>
           </li>
         ))}
       </ul>
